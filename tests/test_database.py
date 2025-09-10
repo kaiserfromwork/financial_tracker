67965955fda6d52f1027bf9f6e33ee0c1d5029fe
Kaiser <kaiserfromwork@gmail.com>
2025-09-10 01:38:28 +0100

    refactor(user): move user table logic to UserTable
    Moved user-table related logic to a new class UserTable.
    Implemented a database attribute for easier access, to
    database. Also, updated test cases to reflect changes.

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,15 +3,14 @@ import pytest
 import os
 
 from src.database import Database
+from src.user_table import UserTable
 
 
 @pytest.fixture
-def test_db_conn(tmp_path):
+def database(tmp_path):
     db_file = tmp_path / "my_database.db"
     db = Database(str(db_file))
-    conn = db._connect()
-    yield conn
-    conn.close()
+    return db
 
 
 def delete_temp_db(temp_db):
@@ -19,28 +18,16 @@ def delete_temp_db(temp_db):
         os.remove(temp_db)
 
 
-def test_connect(test_db_conn):
-    conn = test_db_conn
-    assert isinstance(conn, sqlite3.Connection)
-
-    cur = conn.cursor()
-    cur.execute("SELECT 1;")
-    result = cur.fetchone()
-    assert result[0] == 1
-
-    conn.close()
-
-
-def test_add_user(tmp_path):
-    db_file = tmp_path / "my_database.db"
-    db = Database(str(db_file))
+def test_add_user(database):
+    db = database
+    user_table = UserTable(database)
     with db._connect() as conn:
         assert isinstance(conn, sqlite3.Connection)
         cur = conn.cursor()
         cur.execute(
             "CREATE TABLE user (user_id INTEGER PRIMARY KEY, user_name, user_email, password_hashed, salt , registration_date)"
         )
-        db.add_user(
+        user_table.add_user(
             "Lucas", "my_email@mail.com", "password_salted", "password_salt", "12-12-12"
         )
         cur.execute("SELECT * FROM user")
@@ -50,9 +37,9 @@ def test_add_user(tmp_path):
         delete_temp_db("my_database.db")
 
 
-def test_delete_user(tmp_path):
-    db_file = tmp_path / "my_database.db"
-    db = Database(str(db_file))
+def test_delete_user(database):
+    db = database
+    user_table = UserTable(database)
     with db._connect() as conn:
         assert isinstance(conn, sqlite3.Connection)
 
@@ -61,37 +48,41 @@ def test_delete_user(tmp_path):
             CREATE TABLE user (user_id INTEGER PRIMARY KEY, user_name, user_email, password_hashed, salt, registration_date)
         """)
 
-        db.add_user("Lucas", "my_email@mail.com", "password_salted", "salt", "20-20-20")
-        delete_row = db.delete_user("Not-a-user", "210293")
+        user_table.add_user(
+            "Lucas", "my_email@mail.com", "password_salted", "salt", "20-20-20"
+        )
+        delete_row = user_table.delete_user("Not-a-user", "210293")
         assert delete_row is False
 
-        delete_user = db.delete_user("Lucas", "my_email@mail.com")
+        delete_user = user_table.delete_user("Lucas", "my_email@mail.com")
         assert cur.rowcount == -1
         assert delete_user is True
 
-        delete_test = db.delete_user("Lucas", "my_email@mail.com")
+        delete_test = user_table.delete_user("Lucas", "my_email@mail.com")
         assert delete_test is False
 
         delete_temp_db("my_database.db")
 
 
-def test_select_user(tmp_path):
-    db_file = tmp_path / "my_database.db"
-    db = Database(str(db_file))
+def test_select_user(database):
+    db = database
+    user_table = UserTable(database)
     with db._connect() as conn:
         cur = conn.cursor()
 
         cur.execute("""
             CREATE TABLE user (user_id INTEGER PRIMARY KEY, user_name, user_email, password_hashed, salt, registration_date)
         """)
-        db.add_user("Lucas", "my_email@mail.com", "password_salted", "salt", "20-20-20")
+        user_table.add_user(
+            "Lucas", "my_email@mail.com", "password_salted", "salt", "20-20-20"
+        )
 
         user_info = {
             "user_name": "Lucas",
             "user_email": "my_email@mail.com",
             "registration_date": "20-20-20",
         }
-        result = db.select_user(user_info)
+        result = user_table.select_user(user_info)
         assert isinstance(result, list)
 
         user_id, user_name, user_email = result[0]
@@ -104,7 +95,7 @@ def test_select_user(tmp_path):
             "user_email": "my_mail@mail.com",
             "registration_date": "10-20-20",
         }
-        result = db.select_user(wrong_user_info)
+        result = user_table.select_user(wrong_user_info)
         assert result is None
 
         delete_temp_db("my_database.db")
